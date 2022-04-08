78c9a24a3c63181e8ce4d14dccf50b36f2f7da1b
ohirumaru <lunchmaru250@gmail.com>
2022-04-09 02:35:25 +0900

    user_id表示を消去

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def handle_message(event):
         co=temp[1]
         line_bot_api.reply_message(
             event.reply_token,
-            [TextSendMessage(text='以下の内容で登録します'), TextSendMessage(text=f"日付:{temp[0]}, 予定名:{co}, 時刻{temp[2]}, user_id:{event.source.user_id}")])
+            [TextSendMessage(text='以下の内容で登録します'), TextSendMessage(text=f"日付:{temp[0]}, 予定名:{co}, 時刻{temp[2]}")])
         conn=sqlite3.connect('schedule.db')
         c=conn.cursor()
         user_id=event.source.user_id
