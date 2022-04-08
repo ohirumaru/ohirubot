307be8cd6f9e7c0ad8b94bd9024a4b802bcbabed
ohirumaru <lunchmaru250@gmail.com>
2022-04-09 02:26:07 +0900

    動いて...

--- reminder.py
+++ reminder.py
@@ -1,3 +1,12 @@
+# 環境変数の読み込み(実機の場合)
+# import config
+
+import sqlite3
+
+# 環境変数取得(実機の場合)
+# CHANNEL_ACCESS_TOKEN = config.CHANNEL_ACCESS_TOKEN
+# CHANNEL_SECRET = config.CHANNEL_SECRET
+
 from flask import Flask, request, abort
 import os
 
@@ -21,6 +30,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     line_bot_api.broadcast(TextSendMessage(text='今日の予定をお知らせします!!'))
+    # line_bot_api.push_message("Ue62ad6ba175451424f3562fc59a3f30f", TextSendMessage(text=f"{3+3}月"))
     conn=sqlite3.connect('schedule.db')
     c=conn.cursor()
     for a in c.execute("select * from schedules"):
