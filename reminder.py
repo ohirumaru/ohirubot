7ab02a06020eb2c9a708fa6ec54df225805ac916
ohirumaru <lunchmaru250@gmail.com>
2022-04-10 23:28:21 +0900

    a

--- reminder.py
+++ reminder.py
@@ -34,8 +34,9 @@ def main():
     conn=sqlite3.connect('schedule.db')
     c=conn.cursor()
     for a in c.execute("select * from schedules"):
-	    line_bot_api.push_message(a[3], TextSendMessage(text=f"{a[1]}に{a[2]}の予定です!")) #(1,'鈴木','suzuki')(2,'田中','tanaka')(3,'竹田','takeda')
-    conn.commit()
+	    # line_bot_api.push_message(a[3], TextSendMessage(text=f"{a[1]}に{a[2]}の予定です!")) #(1,'鈴木','suzuki')(2,'田中','tanaka')(3,'竹田','takeda')
+        line_bot_api.push_message("Ue62ad6ba175451424f3562fc59a3f30f", TextSendMessage(text=f"{a[1]}に{a[2]}の予定です!"))
+    # conn.commit()
     conn.close()
     
 if __name__ == "__main__":
